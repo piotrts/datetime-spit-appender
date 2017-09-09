11f950423123b3f996051ee62785c4b8d8821280
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-09-09 18:20:10 +0200

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject piotrts/datetime-spit-appender "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "DateTime spit appender for Timbre"
+  :url "https://github.com/piotrts/datetime-spit-appender"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
